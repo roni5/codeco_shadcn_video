f07833dd97cd079f43bea4674a30724c2e767813
Roni McGuinness <roniemail1@gmail.com>
2025-11-25 21:59:08 +0000

    test commit3

---
+++ lib/GoogleTagManager.tsx
@@ -0,0 +1,30 @@
+import Script from 'next/script';
+
+type Props = { gtmId: string };
+
+export function GoogleTagManager({ gtmId }: Props) {
+  if (!gtmId) return null;
+
+  return (
+    <>
+      <Script id="gtm-init" strategy="afterInteractive">
+        {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','${gtmId}');
+        `}
+      </Script>
+
+      <noscript>
+        <iframe
+          src={`https://www.googletagmanager.com/ns.html?id=${gtmId}`}
+          height="0"
+          width="0"
+          style={{ display: 'none', visibility: 'hidden' }}
+        />
+      </noscript>
+    </>
+  );
+}
